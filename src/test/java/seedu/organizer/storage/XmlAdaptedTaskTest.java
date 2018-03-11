73242c8b6e781e3883fe1e5c96c211705aa90c70
Guek Ling <yeoyueling@gmail.com>
2018-03-11 12:24:04 +0800

    Refactor Task#Email -> Task#Deadline in XmlAdaptedTaskTest.java

--- src/test/java/seedu/organizer/storage/XmlAdaptedTaskTest.java
+++ src/test/java/seedu/organizer/storage/XmlAdaptedTaskTest.java
@@ -26,7 +26,7 @@ public class XmlAdaptedTaskTest {
 
     private static final String VALID_NAME = BENSON.getName().toString();
     private static final String VALID_PHONE = BENSON.getPhone().toString();
-    private static final String VALID_EMAIL = BENSON.getEmail().toString();
+    private static final String VALID_EMAIL = BENSON.getDeadline().toString();
     private static final String VALID_ADDRESS = BENSON.getAddress().toString();
     private static final List<XmlAdaptedTag> VALID_TAGS = BENSON.getTags().stream()
             .map(XmlAdaptedTag::new)
