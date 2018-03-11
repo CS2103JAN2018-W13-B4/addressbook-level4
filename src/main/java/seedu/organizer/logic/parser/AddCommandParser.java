ad1114f84a1bf444b1bcb416f4d85a20170ad930
Guek Ling <yeoyueling@gmail.com>
2018-03-11 12:38:08 +0800

    Fix typo

--- src/main/java/seedu/organizer/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/organizer/logic/parser/AddCommandParser.java
@@ -44,7 +44,7 @@ public class AddCommandParser implements Parser<AddCommand> {
         try {
             Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME)).get();
             Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE)).get();
-            Deadline deadline = ParserUtil.parseDeadline(argMultimap.getValue(PREFIX_DEADLINE).get();
+            Deadline deadline = ParserUtil.parseDeadline(argMultimap.getValue(PREFIX_DEADLINE)).get();
             Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS)).get();
             Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
