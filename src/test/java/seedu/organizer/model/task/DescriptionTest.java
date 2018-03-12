05392ca767643d67ebcf45918fd9302716e3c1ec
Guek Ling <yeoyueling@gmail.com>
2018-03-12 18:54:23 +0800

    Edit & Add tests accordingly

--- src/test/java/seedu/organizer/model/task/DescriptionTest.java
+++ src/test/java/seedu/organizer/model/task/DescriptionTest.java
@@ -1,6 +1,5 @@
 package seedu.organizer.model.task;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,12 +11,6 @@ public class DescriptionTest {
     @Test
     public void constructor_null_throwsNullPointerException() {
         Assert.assertThrows(NullPointerException.class, () -> new Description(null));
-    }
-
-    @Test
-    public void constructor_invalidDescription_throwsIllegalArgumentException() {
-        String invalidDescription = "";
-        Assert.assertThrows(IllegalArgumentException.class, () -> new Description(invalidDescription));
     }
 
     @Test
