2e97dd716ff652076503841a1a7ddc7b0a16450b
Agus Sentosa Hermawan <agus.sentosa787@gmail.com>
2018-03-13 14:26:54 +0800

    Add testcase for Status

--- src/main/java/seedu/organizer/model/task/Status.java
+++ src/main/java/seedu/organizer/model/task/Status.java
@@ -1,5 +1,7 @@
 package seedu.organizer.model.task;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Represents a Task's status in the organizer book.
  */
---
+++ src/test/java/seedu/organizer/model/task/StatusTest.java
@@ -0,0 +1,40 @@
+package seedu.organizer.model.task;
+
+import org.junit.Test;
+import seedu.organizer.testutil.Assert;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class StatusTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Status(null));
+    }
+
+    @Test
+    public void comparator_allCombination() {
+        Status s_true_1 = new Status(true);
+        Status s_true_2 = new Status(true);
+        Status s_false_1 = new Status(false);
+        Status s_false_2 = new Status(false);
+
+        assertTrue(s_true_1.equals(s_true_1));
+        assertTrue(s_true_1.equals(s_true_2));
+        assertFalse(s_true_1.equals(s_false_1));
+
+        assertTrue(s_false_1.equals(s_false_1));
+        assertTrue(s_false_1.equals(s_false_2));
+        assertFalse(s_false_1.equals(s_true_1));
+    }
+
+    public void getInverse_allCombination() {
+        Status s_true = new Status(true);
+        Status s_false = new Status(false);
+
+        assertEquals(s_true.getInverse(), s_false);
+        assertEquals(s_false.getInverse(), s_true);
+    }
+}
