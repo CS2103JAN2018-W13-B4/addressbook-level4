fe61f5d5c3bf06a5fbf3f4e09c3f0be701d1ae70
Guek Ling <yeoyueling@gmail.com>
2018-03-25 19:35:26 +0800

    New PreviousMonthCommand.java

---
+++ src/main/java/seedu/organizer/logic/commands/PreviousMonthCommand.java
@@ -0,0 +1,24 @@
+package seedu.organizer.logic.commands;
+
+import java.time.YearMonth;
+
+import seedu.organizer.ui.CalendarPanel;
+
+/**
+ * ADD COMMENTS!!!
+ * Lists all persons in the organizer book to the user.
+ */
+public class PreviousMonthCommand extends Command {
+
+    public static final String COMMAND_WORD = "pmonth";
+    public static final String COMMAND_ALIAS = "pm";
+
+    @Override
+    public CommandResult execute() {
+        CalendarPanel calendarPanel = new CalendarPanel();
+        YearMonth viewYearMonth = calendarPanel.getYearMonth().minusMonths(1);
+        calendarPanel.loadPreviousMonthView(viewYearMonth);
+        return new CommandResult("TEST");
+    }
+
+}
