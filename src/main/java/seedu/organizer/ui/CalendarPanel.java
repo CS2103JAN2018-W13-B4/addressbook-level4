ee62b53299978ee6e15ad953cf3acb9afef737ef
Guek Ling <yeoyueling@gmail.com>
2018-03-25 11:54:41 +0800

    Fix checkstyle issues

--- src/main/java/seedu/organizer/ui/CalendarPanel.java
+++ src/main/java/seedu/organizer/ui/CalendarPanel.java
@@ -63,6 +63,6 @@ public class CalendarPanel extends UiPart<Region> {
      * @throws IOException if there's problem fetching the Calendar.
      */
     private void createMonthView(YearMonth yearMonth) throws IOException {
-       calendar.getMonthView(yearMonth);
+        calendar.getMonthView(yearMonth);
     }
 }
