e3b518e30c9e389f51838f67f216cc7d2f86d179
Guek Ling <yeoyueling@gmail.com>
2018-03-30 12:12:21 +0800

    Add new MonthViewHandle & MonthViewTest

---
+++ src/test/java/seedu/organizer/ui/MonthViewTest.java
@@ -0,0 +1,219 @@
+package seedu.organizer.ui;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.organizer.testutil.TypicalExecutedCommands.PREVIOUS_MONTH_COMMAND_ALIAS;
+import static seedu.organizer.testutil.TypicalExecutedCommands.PREVIOUS_MONTH_COMMAND_WORD;
+import static seedu.organizer.testutil.TypicalExecutedCommands.getTypicalExecutedCommands;
+import static seedu.organizer.testutil.TypicalTasks.getTypicalTasks;
+
+import java.time.YearMonth;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import guitests.guihandles.MonthViewHandle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.ListView;
+import seedu.organizer.model.task.Task;
+import seedu.organizer.testutil.TaskBuilder;
+import seedu.organizer.ui.calendar.EntryCard;
+import seedu.organizer.ui.calendar.MonthView;
+
+//@@author guekling
+public class MonthViewTest extends GuiUnitTest {
+    private static final ObservableList<Task> TYPICAL_TASKS = FXCollections.observableList(getTypicalTasks());
+    private static final ObservableList<String> TYPICAL_EXECUTED_COMMANDS = FXCollections.observableList
+        (getTypicalExecutedCommands());
+
+    private static final YearMonth MAY_2018 = YearMonth.of(2018, 5);
+    private static final YearMonth DEC_2018 = YearMonth.of(2018, 12);
+
+    private MonthView monthView;
+    private MonthViewHandle monthViewHandle;
+
+    @Before
+    public void setUp() {
+        monthView = new MonthView(TYPICAL_TASKS, TYPICAL_EXECUTED_COMMANDS);
+        uiPartRule.setUiPart(monthView);
+
+        monthViewHandle = new MonthViewHandle(monthView.getRoot());
+    }
+
+    @Test
+    public void display_fiveWeeksCalendar() {
+        monthView.getMonthView(MAY_2018);
+        guiRobot.pauseForHuman();
+
+        // verify that calendar title is displayed correctly
+        monthViewHandle.getCalendarTitleText();
+        String expectedTitle = "MAY 2018";
+        assertEquals(expectedTitle, monthViewHandle.getCalendarTitleText());
+
+        // verify that the first date of the month is displayed in the correct row and column
+        Node startDateNode = monthViewHandle.getPrintedDateNode(1);
+        int startDateRow = monthViewHandle.getRowIndex(startDateNode);
+        int startDateColumn = monthViewHandle.getColumnIndex(startDateNode);
+
+        assertEquals(0, startDateRow);
+        assertEquals(2, startDateColumn);
+
+        // verify that the last date of the month is displayed in the correct row and column
+        Node lastDateNode = monthViewHandle.getPrintedDateNode(31);
+        int lastDateRow = monthViewHandle.getRowIndex(lastDateNode);
+        int lastDateColumn = monthViewHandle.getColumnIndex(lastDateNode);
+
+        assertEquals(4, lastDateColumn);
+        assertEquals(4, lastDateRow);
+    }
+
+    @Test
+    public void display_sixWeeksCalendar() {
+        monthView.getMonthView(DEC_2018);
+        guiRobot.pauseForHuman();
+
+        // verify that calendar title is displayed correctly
+        monthViewHandle.getCalendarTitleText();
+        String expectedTitle = "DECEMBER 2018";
+        assertEquals(expectedTitle, monthViewHandle.getCalendarTitleText());
+
+        // verify that the last date of the month is displayed in the correct row and column
+        Node lastDateNode = monthViewHandle.getPrintedDateNode(31);
+        int lastDateRow = monthViewHandle.getRowIndex(lastDateNode);
+        int lastDateColumn = monthViewHandle.getColumnIndex(lastDateNode);
+
+        assertEquals(1, lastDateColumn);
+        assertEquals(0, lastDateRow);
+    }
+
+    @Test
+    public void showEntries_fiveWeeksCalendar() {
+        monthView.getMonthView(MAY_2018);
+        guiRobot.pauseForHuman();
+
+        // one entry
+        Task toAddTask = new TaskBuilder().withName("ES2660").withDeadline("2018-05-01").build();
+        addTaskToTaskList(toAddTask);
+        guiRobot.pauseForHuman();
+
+        ListView<EntryCard> entriesListView = (ListView) monthViewHandle.getListViewEntriesNode(0, 2);
+        EntryCard actualEntryCard = entriesListView.getItems().get(0);
+        EntryCard expectedEntryCard = new EntryCard(toAddTask);
+        assertEquals(expectedEntryCard, actualEntryCard);
+
+        // multiple entries on different dates
+        toAddTask = new TaskBuilder().withName("CS2101").withDeadline("2018-05-18").build();
+        addTaskToTaskList(toAddTask);
+        guiRobot.pauseForHuman();
+
+        entriesListView = (ListView) monthViewHandle.getListViewEntriesNode(2, 5);
+        actualEntryCard = entriesListView.getItems().get(0);
+        expectedEntryCard = new EntryCard(toAddTask);
+        assertEquals(expectedEntryCard, actualEntryCard);
+
+        toAddTask = new TaskBuilder().withName("GEQ1000").withDeadline("2018-05-23").build();
+        addTaskToTaskList(toAddTask);
+        guiRobot.pauseForHuman();
+
+        entriesListView = (ListView) monthViewHandle.getListViewEntriesNode(3, 3);
+        actualEntryCard = entriesListView.getItems().get(0);
+        expectedEntryCard = new EntryCard(toAddTask);
+        assertEquals(expectedEntryCard, actualEntryCard);
+
+        // entries on the same date
+        toAddTask = new TaskBuilder().withName("MA1101R").withDeadline("2018-05-18").build();
+        addTaskToTaskList(toAddTask);
+        guiRobot.pauseForHuman();
+
+        entriesListView = (ListView) monthViewHandle.getListViewEntriesNode(2, 5);
+        actualEntryCard = entriesListView.getItems().get(1);
+        expectedEntryCard = new EntryCard(toAddTask);
+        assertEquals(expectedEntryCard, actualEntryCard);
+    }
+
+    @Test
+    public void showEntries_sixWeeksCalendar() {
+        monthView.getMonthView(DEC_2018);
+        guiRobot.pauseForHuman();
+
+        Task toAddTask = new TaskBuilder().withName("CS2103T").withDeadline("2018-12-31").build();
+        addTaskToTaskList(toAddTask);
+        guiRobot.pauseForHuman();
+
+        ListView<EntryCard> entriesListView = (ListView) monthViewHandle.getListViewEntriesNode(0, 1);
+        EntryCard actualEntryCard = entriesListView.getItems().get(0);
+        EntryCard expectedEntryCard = new EntryCard(toAddTask);
+        assertEquals(expectedEntryCard, actualEntryCard);
+    }
+
+    @Test
+    public void goToPreviousMonth_commandsSuccessful() {
+        monthView.getMonthView(MAY_2018);
+
+        addCommandToExecutedCommandsList(PREVIOUS_MONTH_COMMAND_WORD);
+
+        // verify that command is successful by ensuring calendar title is displayed correctly
+        monthViewHandle.getCalendarTitleText();
+        String expectedTitle = "APRIL 2018";
+        assertEquals(expectedTitle, monthViewHandle.getCalendarTitleText());
+
+        addCommandToExecutedCommandsList(PREVIOUS_MONTH_COMMAND_ALIAS);
+
+        // verify that command is successful by ensuring that calendar title is displayed correctly
+        monthViewHandle.getCalendarTitleText();
+        expectedTitle = "MARCH 2018";
+        assertEquals(expectedTitle, monthViewHandle.getCalendarTitleText());
+    }
+
+    @Test
+    public void goToPreviousMonth_titleAndDatesPrintedSuccessfully() {
+        monthView.getMonthView(MAY_2018);
+
+        addCommandToExecutedCommandsList(PREVIOUS_MONTH_COMMAND_WORD);
+        guiRobot.pauseForHuman();
+
+        // verify that calendar title is displayed correctly
+        monthViewHandle.getCalendarTitleText();
+        String expectedTitle = "APRIL 2018";
+        assertEquals(expectedTitle, monthViewHandle.getCalendarTitleText());
+
+        // verify that grid lines are visible after clearCalendar() is called
+        assertEquals(true, monthViewHandle.isGridLinesVisible());
+
+        // verify that previous dates have been cleared after clearCalendar() is called
+        Node node = monthViewHandle.getNode(1);
+        int column = monthViewHandle.getColumnIndex(node);
+        int row = monthViewHandle.getRowIndex(node);
+
+        assertEquals(0, column);
+        assertEquals(0, row);
+
+        // verify that the first date of the month is displayed in the correct row and column
+        Node startDateNode = monthViewHandle.getPrintedDateNode(1);
+        int startDateRow = monthViewHandle.getRowIndex(startDateNode);
+        int startDateColumn = monthViewHandle.getColumnIndex(startDateNode);
+
+        assertEquals(0, startDateRow);
+        assertEquals(0, startDateColumn);
+
+        // verify that the last date of the month is displayed in the correct row and column
+        Node lastDateNode = monthViewHandle.getPrintedDateNode(30);
+        int lastDateRow = monthViewHandle.getRowIndex(lastDateNode);
+        int lastDateColumn = monthViewHandle.getColumnIndex(lastDateNode);
+
+        assertEquals(1, lastDateColumn);
+        assertEquals(4, lastDateRow);
+    }
+
+    /**
+     * Adds a new {@code command} to the {@code TYPICAL_EXECUTED_COMMANDS} observable list.
+     */
+    private void addCommandToExecutedCommandsList(String command) {
+        TYPICAL_EXECUTED_COMMANDS.add(command);
+    }
+
+    private void addTaskToTaskList(Task task) {
+        TYPICAL_TASKS.add(task);
+    }
+}
